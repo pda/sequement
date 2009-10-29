3899e16292b6fc38662b58995c4166e15718f089
Paul Annesley <paul@annesley.cc>
2009-10-29 20:39:11 +1100

    sequement_spec uses before() hook.

--- spec/sequence_spec.rb
+++ spec/sequence_spec.rb
@@ -4,25 +4,22 @@ require 'sequement'
 
 describe Sequement::Sequence do
 
-  def delete_sequence(dir, name)
-    path = '%s/%s' % [dir, name]
+  before :each do
+    path = 'tmp/name'
     File.delete path if File.exists? path
   end
 
 	it "should instantiate" do
-		delete_sequence 'tmp', 'name'
 		seq = Sequement::Sequence.new('name', 'tmp')
 	end
 
 	it "should increment" do
-		delete_sequence 'tmp', 'name'
 		seq = Sequement::Sequence.new('name', 'tmp')
 		seq.next.should == 1
 		seq.next.should == 2
 	end
 
 	it "should persist" do
-		delete_sequence 'tmp', 'name'
 		seq = Sequement::Sequence.new('name', 'tmp')
 		seq.next.should == 1
 		seq.next.should == 2
@@ -33,7 +30,6 @@ describe Sequement::Sequence do
   end
 
   it "should not overlap after crash" do
-		delete_sequence 'tmp', 'name'
 		seq_precrash = Sequement::Sequence.new('name', 'tmp')
 		seq_precrash.next.should == 1
 		seq_precrash.next.should == 2
